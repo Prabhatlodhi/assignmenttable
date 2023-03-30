b54d8ff1c9c216a2d5e08fa51b6f040f3b2448f7
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-03-30 18:32:51 +0530

    aa

--- src/components/InputField.js/InputField.js
+++ src/components/InputField.js/InputField.js
@@ -1,55 +1,129 @@
-import React, { useState } from 'react'
-import TableContent from '../TableContent/TableContent'
+import React, { useState } from 'react';
+import TableContent from '../TableContent/TableContent';
 import "./InputField.css"
 
 const InputField = () => {
-    const [data, setData] = useState("");
-    const [data1, setData1] = useState("");
-    const [data2, setData2] = useState("");
-    const [data3, setData3] = useState("");
-    const [data4, setData4] = useState("");
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    phone: '',
+    address: '',
+    city: '',
+  });
 
-    const [alldata, setalldata] = useState([])
+  const [data, setData] = useState([]);
 
-    
-    function inputData(){
-        console.log(data)
-        if(data==="") return null;
-        setalldata(
-            [...alldata,
-                {
-                    name: data,
-                    lname: data1,
-                    email:data2,
-                    pan:data3,
-                    city:data4
-                }
-            ]
-        )
-        setData("")
-        setData1("")
-        setData2("")
-        setData3("")
-        setData4("")
-        // console.log(alldata)
-        // setValues([alldata, ...values])
+  const [editIndex, setEditIndex] = useState(null);
+
+  const [sortOrder, setSortOrder] = useState('asc'); // default sorting order is ascending
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (formData.name === '') return;
+
+    if (editIndex !== null) {
+      // Update existing data
+      setData((prevData) =>
+        prevData.map((item, index) => {
+          if (index === editIndex) {
+            return formData;
+          }
+          return item;
+        })
+      );
+      setEditIndex(null);
+    } else {
+      // Add new data
+      setData((prevData) => [...prevData, formData]);
     }
+    setFormData({
+      name: '',
+      email: '',
+      phone: '',
+      address: '',
+      city: '',
+    });
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleEdit = (index) => {
+    setEditIndex(index);
+    setFormData(data[index]);
+  };
+
+  const handleDelete = (index) => {
+    setData((prevData) =>
+      prevData.filter((item, i) => {
+        return i !== index;
+      })
+    );
+  };
+
+  const handleSort = () => {
+    const sortedData = [...data].sort((a, b) => {
+      if (sortOrder === 'asc') {
+        return a.name.localeCompare(b.name);
+      } else {
+        return b.name.localeCompare(a.name);
+      }
+    });
+    setData(sortedData);
+    setSortOrder(sortOrder === 'asc' ? 'desc' : 'asc'); // toggle sorting order
+  };
 
   return (
-    <div className='inputWrapper'>
-      <h1 className='Inputheader'>InputField</h1>
-      <div className='inputContainer'>
-        <input type="text" placeholder='First Name' value={data} onChange={(e)=>{setData(e.target.value)}} />
-        <input type="text" placeholder='Last Name' value={data1} onChange={(e)=>{setData1(e.target.value)}}/>
-        <input type="email" placeholder='Email' value={data2} onChange={(e)=>{setData2(e.target.value)}}/>
-        <input type="text" placeholder='PAN Number' value={data3} onChange={(e)=>{setData3(e.target.value)}}/>
-        <input type="City" placeholder='City' value={data4} onChange={(e)=>{setData4(e.target.value)}}/>
-        <button onClick={inputData}>SUBMIT DATA</button>
-      </div>
-     
-      <TableContent inputalldata={alldata}/>
+    <div className="inputWrapper">
+      <h1>Input Field</h1>
+      <form onSubmit={handleSubmit} >
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+        <label>Email:</label>
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+        />
+        <label>Phone:</label>
+        <input
+          type="text"
+          name="phone"
+          value={formData.phone}
+          onChange={handleChange}
+        />
+        <label>Address:</label>
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+        />
+        <label>City:</label>
+        <input
+          type="text"
+          name="city"
+          value={formData.city}
+          onChange={handleChange}
+        />
+        <button type="submit">{editIndex !== null ? 'Update' : 'Submit'}</button>
+      </form>
+
+      <TableContent data={data} handleEdit={handleEdit} handleDelete={handleDelete} handleSort={handleSort} sortOrder={sortOrder} />
     </div>
-  )
-}
+  );
+};
 
 export default InputField;
+
--- src/components/TableContent/TableContent.js
+++ src/components/TableContent/TableContent.js
@@ -1,82 +1,35 @@
-import React, { useState } from "react";
-import "./TableContent.css";
+import React from 'react';
+import './TableContent.css';
 
-const TableContent = ({ inputalldata }) => {
-  const [allpropsData, setallpropsData] = useState(inputalldata);
-  const [searchData, setsearchData] = useState("");
-  const [edit, setEdit] = useState({});
-
-  function filterName(value) {
-    console.log(allpropsData[0].name);
-    if (
-      allpropsData.map((ele, ind) => {
-        ele.name = searchData;
-        console.log("tr");
-      })
-    )
-      setsearchData("");
-  }
-
-   
-
-  function handleEdit(ind) {
-    setEdit({
-      name: allpropsData[ind].name,
-      lname: allpropsData[ind].lname,
-      email: allpropsData[ind].email,
-      pan: allpropsData[ind].pan,
-      city: allpropsData[ind].city,
-    });
-  }
-
-   
-
+const TableContent = ({ data, handleEdit, handleDelete }) => {
   return (
-    <div className="tablewrapper">
-      <div>
-        <input
-          type="text"
-          placeholder="Search By Name"
-          value={searchData}
-          onChange={(e) => {
-            setsearchData(e.target.value);
-          }}
-        />
-        <button onClick={filterName}>Search</button>
-      </div>
-
-      <table>
-        <thead>
-          <tr>
-            <th>
-              First Name 
-            </th>
-            <th>Last Name</th>
-            <th>Email</th>
-            <th>PAN Number</th>
-            <th>City Name</th>
-            <th></th>
+    <table>
+      <thead>
+        <tr>
+          <th>Name <button>Sort a to z</button></th>
+          <th>Email</th>
+          <th>Phone</th>
+          <th>Address</th>
+          <th>City</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {data.map((row, index) => (
+          <tr key={index}>
+            <td>{row.name}</td>
+            <td>{row.email}</td>
+            <td>{row.phone}</td>
+            <td>{row.address}</td>
+            <td>{row.city}</td>
+            <td>
+              <button onClick={() => handleEdit(index)}>Edit</button>
+              <button onClick={() => handleDelete(index)}>Delete</button>
+            </td>
           </tr>
-        </thead>
-        <tbody>
-          {inputalldata.map((items, ind) => {
-            console.log(inputalldata);
-            return (
-              <tr key={ind}>
-                <td>{items.name}</td>
-                <td>{items.lname}</td>
-                <td>{items.email}</td>
-                <td>{items.pan}</td>
-                <td>{items.city}</td>
-                <td>
-                  <button onClick={handleEdit}>Edit</button> 
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    </div>
+        ))}
+      </tbody>
+    </table>
   );
 };
 
