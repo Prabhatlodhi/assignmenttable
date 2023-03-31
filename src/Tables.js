99b6d612a7afa9d3e696e2a62c667c6bd968c322
Prabhatlodhi <lodhi.prabhat@gmail.com>
2023-03-31 21:43:01 +0530

    aa

--- src/Tables.js
+++ src/Tables.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./index.css"
+import "./index.css";
 
 function Tables() {
   const [users, setUsers] = useState([]);
@@ -9,6 +9,11 @@ function Tables() {
   const [panNumber, setPanNumber] = useState("");
   const [mobile, setMobile] = useState("");
   const [selectedUser, setSelectedUser] = useState(null);
+  
+
+  const [searchTerm, setSearchTerm] = useState("");
+const [searchClicked, setSearchClicked] = useState(false);
+
 
   const handleAddUser = (e) => {
     e.preventDefault();
@@ -17,7 +22,7 @@ function Tables() {
       lastName,
       email,
       panNumber,
-      mobile
+      mobile,
     };
     if (selectedUser !== null) {
       const updatedUsers = [...users];
@@ -55,40 +60,77 @@ function Tables() {
     sortedUsers.sort((a, b) => a.name.localeCompare(b.name));
     setUsers(sortedUsers);
   };
+
+  const handleSearchByName = (e) => {
+    setSearchTerm(e.target.value.toLowerCase());
+  };
   
+  const filteredUsers = users.filter(user => user.name.toLowerCase().includes(searchTerm));
+  
+
   return (
     <div className="App">
       <h1>User Details</h1>
       <form onSubmit={handleAddUser}>
         <label>
           Name:
-          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+          <input
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Last Name:
-          <input type="text" value={lastName} onChange={(e) => setLastName(e.target.value)} />
+          <input
+            type="text"
+            value={lastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </label>
         <br />
         <label>
           PAN Number:
-          <input type="text" value={panNumber} onChange={(e) => setPanNumber(e.target.value)} />
+          <input
+            type="text"
+            value={panNumber}
+            onChange={(e) => setPanNumber(e.target.value)}
+          />
         </label>
         <br />
         <label>
           Mobile:
-          <input type="tel" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+          <input
+            type="tel"
+            value={mobile}
+            onChange={(e) => setMobile(e.target.value)}
+          />
         </label>
         <br />
-        <button type="submit">{selectedUser !== null ? "Update User" : "Add User"}</button>
+        <button type="submit">
+          {selectedUser !== null ? "Update User" : "Add User"}
+        </button>
       </form>
       <button onClick={handleSortByName}>Sort by Name</button>
       <br />
+      <div className="inputbox">
+      <input type="text" value={searchTerm} onChange={handleSearchByName} placeholder="Search by name" />
+      <button onClick={() => {
+        setSearchTerm("");
+        setSearchClicked(false);
+      }}>Clear</button>
+      <button onClick={() => setSearchClicked(true)}>Search</button>
+    </div>
       <table>
         <thead>
           <tr>
@@ -102,7 +144,7 @@ function Tables() {
           </tr>
         </thead>
         <tbody>
-          {users.map((user, index) => (
+        {filteredUsers.map((user, index) => (
             <tr key={index}>
               <td>{user.name}</td>
               <td>{user.lastName}</td>
@@ -111,19 +153,14 @@ function Tables() {
               <td>{user.mobile}</td>
               <td>
                 <button onClick={() => handleEditUser(index)}>Edit</button>
-              </td>
-              <td>
                 <button onClick={() => handleDeleteUser(index)}>Delete</button>
               </td>
             </tr>
           ))}
         </tbody>
       </table>
- 
-
-
-</div>
-);
+    </div>
+  );
 }
 
 export default Tables;
